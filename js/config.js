ce0a27264363d16bc8cdc0525d90c31e7aef96f3
vishal Biradar <108792421+vishal001-tec@users.noreply.github.com>
2025-01-20 22:37:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_mcZoMnget', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '30onjbq7fb8u3nttr0uj1t87v8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
